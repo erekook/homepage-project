100405159af5756b64653f40cef63a4295053a40
longjietan <longjietan122@gmail.com>
2019-11-29 18:06:12 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,39 @@
+// vue.config.js
+module.exports = {
+    // 默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上，例如 https://www.my-app.com/。
+    // 如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。
+    // 例如，如果你的应用被部署在 https://www.my-app.com/my-app/，则设置 publicPath 为 /my-app/。
+    publicPath: process.env.NODE_ENV === 'production' ? '/prms/' : '/',
+    // 当运行 vue-cli-service build 时生成的生产环境构建文件的目录
+    outputDir: 'dist',
+    // 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+    assetsDir: './assets',
+    // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
+    indexPath: 'index.html',
+    pages: {
+        index: {
+            // page 的入口
+            entry: 'src/main.js',
+            // 模板来源
+            template: 'public/index.html',
+            // 在 dist/index.html 的输出
+            filename: 'index.html',
+            // 当使用 title 选项时，
+            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+            title: 'Index Page',
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+    },
+    lintOnSave: true,
+    // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+    productionSourceMap: true,
+    devServer: {
+        overlay: {
+            warnings: true,
+            errors: true
+        }
+    }
+
+}
